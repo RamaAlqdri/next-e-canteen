a4620715d456b37aba70fbb0279310a86df72a95
RamaAlqdri <ramadhanialqadri12@gmail.com>
2024-02-27 01:47:05 +0700

    responsive product item

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -49,7 +49,7 @@ export default async function Home() {
       </div>
 
       <h2 className="text-2xl py-2">Latest Product</h2>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+      <div className="grid grid-cols-1 gap-2 sm:gap-4 md:grid-cols-3 lg:grid-cols-4">
         {latestProducts.map((product) => (
           <ProductItem key={product.slug} product={convertDocToObj(product)} />
         ))}
--- components/products/ProductItem.tsx
+++ components/products/ProductItem.tsx
@@ -8,39 +8,41 @@ import { convertDocToObj } from "@/lib/utils";
 
 export default function ProductItem({ product }: { product: Product }) {
   return (
-    <div className="card bg-base-200  mb-4">
-      <figure>
-        <Link href={`/product/${product.slug}`}>
-          <Image
-            src={product.image}
-            alt={product.name}
-            width={300}
-            height={300}
-            className="object-cover h-64 w-full"
-          />
-        </Link>
-      </figure>
-      <div className="card-body">
-        <Link href={`/product/${product.slug}`}>
-          <h2 className="card-title font-normal ">{product.name}</h2>
-        </Link>
-        <p className="mb-2">{product.brand}</p>
-        <div className="flex justify-between">
-          <div className="card-actions flex items-center justify-between">
-            <span className="text-2xl font-semibold">
-              Rp {formatRupiah(product.price)},00
-            </span>
-            {"  "}
-          </div>
-          <div>
-            <AddToCart
-              item={{
-                ...convertDocToObj(product),
-                qty: 0,
-                color: "",
-                size: "",
-              }}
+    <div className="card bg-base-200 mb-0 sm:mb-4 ">
+      <div className="flex sm:grid">
+        <div className="h-32 w-32 sm:h-96 sm:w-full m-2 sm:m-0">
+          <Link href={`/product/${product.slug}`}>
+            <Image
+              src={product.image}
+              alt={product.name}
+              width={300}
+              height={300}
+              className="object-cover sm:h-96 h-32 w-32 rounded-2xl sm:w-full"
             />
+          </Link>
+        </div>
+        <div className="sm:card-body w-2/3 sm:w-full flex flex-col justify-between m-4 sm:m-0 sm:flex-none sm:justify-normal sm:grid ">
+          <Link href={`/product/${product.slug}`}>
+            <h2 className="card-title font-normal ">{product.name}</h2>
+          </Link>
+          <p className="mb-2">{product.brand}</p>
+          <div className="flex justify-between">
+            <div className="card-actions flex sm:items-center items-end justify-between">
+              <span className="sm:text-2xl text-lg font-semibold">
+                Rp {formatRupiah(product.price)},00
+              </span>
+              {"  "}
+            </div>
+            <div>
+              <AddToCart
+                item={{
+                  ...convertDocToObj(product),
+                  qty: 0,
+                  color: "",
+                  size: "",
+                }}
+              />
+            </div>
           </div>
         </div>
       </div>

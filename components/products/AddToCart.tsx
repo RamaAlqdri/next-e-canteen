bb02636b914992288a13a2f3515f8a98d19a101e
RamaAlqdri <ramadhanialqadri12@gmail.com>
2024-02-27 21:58:50 +0700

    edit product comp

--- components/products/AddToCart.tsx
+++ components/products/AddToCart.tsx
@@ -15,17 +15,19 @@ export default function AddToCart({ item }: { item: OrderItem }) {
     increase(item);
   };
   return existItem ? (
-    <div>
-        <button className="btn btn-ePrimary border-0 " type="button" onClick = {()=> decrease(existItem)}>
+    <div className="">
+        <button className="btn h-2 px-[0.7rem] min-h-8 sm:min-h-12 sm:p-3 text-xs sm:text-base btn-ePrimary border-0 " type="button" onClick = {()=> decrease(existItem)}>
             -
         </button>
-        <span className="px-2 font-semibold ">{existItem.qty}</span>
-        <button className="btn btn-ePrimary border-0" type="button" onClick={()=> increase(existItem)}>+</button> 
+        <span className="px-2 font-semibold text-xs sm:text-base ">{existItem.qty}</span>
+        <button className="btn h-2 px-[0.7rem] min-h-8 sm:min-h-12 sm:p-3 text-xs sm:text-base btn-ePrimary border-0" type="button" onClick={()=> increase(existItem)}>
+          +
+        </button> 
     </div>
 
   ):(
     <button
-        className="btn btn-ePrimary border-0 w-full drop-shadow-[0_10px_10px_rgba(237,161,71,0.4)]"
+        className="btn h-2 px-2 min-h-8 sm:min-h-12 sm:p-3 btn-ePrimary border-0 drop-shadow-[0_10px_10px_rgba(237,161,71,0.4)] text-xs sm:text-base"
         type="button"
         onClick={addToCartHandler}
     >
--- components/products/ProductItem.tsx
+++ components/products/ProductItem.tsx
@@ -8,27 +8,27 @@ import { convertDocToObj } from "@/lib/utils";
 
 export default function ProductItem({ product }: { product: Product }) {
   return (
-    <div className="card bg-base-200 mb-0 sm:mb-4 ">
+    <div className="card bg-base-200 mb-0 sm:mb-4  ">
       <div className="flex sm:grid">
-        <div className="h-32 w-32 sm:h-96 sm:w-full m-2 sm:m-0">
+        <div className="h-24 w-24 sm:h-96 sm:w-full m-2 sm:m-0">
           <Link href={`/product/${product.slug}`}>
             <Image
               src={product.image}
               alt={product.name}
               width={300}
               height={300}
-              className="object-cover sm:h-96 h-32 w-32 rounded-2xl sm:w-full"
+              className="object-cover sm:h-96 h-24 w-24 rounded-2xl sm:w-full"
             />
           </Link>
         </div>
         <div className="sm:card-body w-2/3 sm:w-full flex flex-col justify-between m-4 sm:m-0 sm:flex-none sm:justify-normal sm:grid ">
           <Link href={`/product/${product.slug}`}>
-            <h2 className="card-title font-normal ">{product.name}</h2>
+            <h2 className="card-title font-normal sm:text-base text-sm ">{product.name}</h2>
           </Link>
-          <p className="mb-2">{product.brand}</p>
+          <p className="mb-2 sm:text-base text-xs">{product.brand}</p>
           <div className="flex justify-between">
             <div className="card-actions flex sm:items-center items-end justify-between">
-              <span className="sm:text-2xl text-lg sm:font-semibold font-medium">
+              <span className="sm:text-2xl text-base sm:font-semibold font-medium">
                 Rp {formatRupiah(product.price)},00
               </span>
               {"  "}
